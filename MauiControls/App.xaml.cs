e82fd350892196fad5f989f65039fab130bfd986
Rafael Ferreira <Rafael.Ferreira.35139@formandos.cinel.pt>
2024-12-13 15:50:11 +0000

    Edit Controls

--- MauiControls/App.xaml.cs
+++ MauiControls/App.xaml.cs
@@ -8,7 +8,7 @@ namespace MauiControls
         {
             InitializeComponent();
 
-            MainPage = new MauiSetValueControls();
+            MainPage = new MauiEditControls();
         }
     }
 }
