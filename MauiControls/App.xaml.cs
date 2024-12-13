1cded0f24a52aa47e523bc80b38a9e1c8c6bffd2
Rafael Ferreira <Rafael.Ferreira.35139@formandos.cinel.pt>
2024-12-13 15:31:35 +0000

    Presentation Controls

--- MauiControls/App.xaml.cs
+++ MauiControls/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace MauiControls
+﻿using MauiControls.Pages;
+
+namespace MauiControls
 {
     public partial class App : Application
     {
@@ -6,7 +8,7 @@
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            MainPage = new MauiPresentationControls();
         }
     }
 }
